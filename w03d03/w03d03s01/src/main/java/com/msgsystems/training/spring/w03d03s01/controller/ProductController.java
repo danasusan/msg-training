4a4be4fdbd0ae8156be6385e0f8b322ff3151b91
Bogdan Solga <bogdan.solga@gmail.com>
2016-10-05 10:59:58 +0300

    [dev] Intro methods for RequestParam, PathVariable and RequestBody (using POST)

--- w03d03/w03d03s01/src/main/java/com/msgsystems/training/spring/w03d03s01/controller/ProductController.java
+++ w03d03/w03d03s01/src/main/java/com/msgsystems/training/spring/w03d03s01/controller/ProductController.java
@@ -3,11 +3,18 @@ package com.msgsystems.training.spring.w03d03s01.controller;
 import com.msgsystems.training.spring.w03d03s01.model.Product;
 import com.msgsystems.training.spring.w03d03s01.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
+
+@Lazy
 @Controller
 public class ProductController {
 
@@ -24,9 +31,35 @@ public class ProductController {
 
     @RequestMapping(
             method = RequestMethod.GET,
-            path = "/hello"
+            path = {"/hello", "/salut", "bonjour"}
     )
     public @ResponseBody String helloSpring() {
-        return "Hello, Spring web!";
+        return "Hello, Spring web! The date is " + new Date();
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "/requestParams"
+    )
+    public @ResponseBody String requestParamsIntro(@RequestParam(name = "culoare") String color,
+                                                   @RequestParam(required = false) String weight) {
+        return "The color is '" + color + "', the weight is '" + weight + "'";
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "/pathVariable/{first}/{second}"
+    )
+    public @ResponseBody String pathVariableIntro(@PathVariable String first,
+                                                  @PathVariable String second) {
+        return "The first path variable value is '" + first + "', the second is '" + second + "'";
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            path = "/postIntro"
+    )
+    public @ResponseBody String postIntro(@RequestBody String request) {
+        return "I agree :)";
     }
 }
